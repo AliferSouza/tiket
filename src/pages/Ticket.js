f9e6cc72d2004b13a84d3b893c83581492ba4433
AliferSouza <50971218+AliferSouza@users.noreply.github.com>
2022-06-21 18:34:02 -0300

    Roteamento usuário no banco local

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -1,5 +1,6 @@
 import dadosURL from '../lib/resolverUrlParamentro.js'
 const dadosUrl = dadosURL()
+var [url1, url2] = location.href.split("?")
 export default function Tiket() {
     const props = "toke"
     const toke = JSON.parse(localStorage.getItem('toke') || "[]")
@@ -14,77 +15,57 @@ export default function Tiket() {
 
     }
 
+    const verificarValor = localStorage.hasOwnProperty("indetificacaoTicket")
 
 
-    if (toke.length == 30) {
 
+    if (verificarValor) {
+        return `       
+        <img class="imgLogo2" src="./src/img/logo.png" height="400px">
+             <div class="containerTicket">
+                 <div class="conteinerImgmiasInformacoes">
+                 <div class="conteinerImg">              
+                 <img class="conteinerImgFormat" src="${dadosUrl.Foto}" height="100">                          
+             </div>
 
-        return `
-        
-
-    <img class="imgLogoP" src="./src/img/logo.png">
-  <div class="containerTicket">
-      <div class="conteinerImgmiasInformacoes">
-      <div class="conteinerImg">
-          <div></div>
-          <img class="conteinerImgFormat" src="${dadosUrl.imagemUrl}" height="100">
-          
-          </div>
-
-          <div class="conteinerInformacoes">
-              <h1 class="conteinerInformacoesColor">BEM - VINDO</h1>
-              <h2 class="conteinerInformacoesColor" >${dadosUrl.Nome}</h2>
-              <h4 class="conteinerInformacoesColor conteinerInformacoesNumero" >Tiket:${dadosUrl.valorTicket}</h4>
-           </div>           
-
-      </div>      
-
-  
-  </div>
-
-  <br>
-  <br>
-      <h4  style="color: #fff; font-size: 12px;"> Pedeu a oportunidade!!!</h4>
-  <br>
- 
-  <br>
- 
-  
-  `
-
-
+                 <div class="conteinerInformacoes">
+                      <h1 class="conteinerInformacoesColor">BEM - VINDO</h1>
+                      <h2 class="conteinerInformacoesColor" >${dadosUrl.Nome} ${dadosUrl.Sobrenome}</h2>
+                      <h4 class="conteinerInformacoesColor conteinerInformacoesNumero" >Tiket:${dadosUrl.valorTicket}</h4>
+                 </div>    
+                 </div> 
+                 </div>
+                <br><br>
+               ${toke.length == 2 ? `<h4 style="color:#fff">Acabou as tentativas!</h4>` : `<h3 >Responda o formulario abaixo></h3>
+               <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwSdocVw4zAEvYDeaLRhBmZwGPdRdz3mU59365NeTJLF5l1A/viewform?embedded=true"
+               width="500" height="300" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" >Carregando…</iframe>`} 
+               <br> `
     } else {
 
 
         return `
         <img class="imgLogo2" src="./src/img/logo.png" height="400px">
-  <div class="containerTicket">
-      <div class="conteinerImgmiasInformacoes">
-      <div class="colar"></div>
-      <div class="conteinerImg">         
-          <img class="conteinerImgFormat" src="${dadosUrl.Foto}" height="100">          
-          </div>
+             <div class="containerTicket">
+                 <div class="conteinerImgmiasInformacoes">    
+                 <div class="conteinerImg">         
+                 <img class="conteinerImgFormat" src="${dadosUrl.Foto}" height="100">          
+            </div>
 
-          <div class="conteinerInformacoes">
-              <h1 class="conteinerInformacoesColor">BEM - VINDO</h1>
-              <h2 class="conteinerInformacoesColor" >${dadosUrl.Nome} ${dadosUrl.Sobrenome}</h2>
-              <h4 class="conteinerInformacoesColor conteinerInformacoesNumero" >Ticket:${dadosUrl.valorTicket}</h4>
-           </div>           
+            <div class="conteinerInformacoes">
+                <h1 class="conteinerInformacoesColor">BEM - VINDO</h1>
+                <h2 class="conteinerInformacoesColor" >${dadosUrl.Nome} ${dadosUrl.Sobrenome}</h2>
+                <h4 class="conteinerInformacoesColor conteinerInformacoesNumero" >Ticket:${dadosUrl.valorTicket}</h4>
+            </div>           
 
       </div>      
+      </div>
 
-  
-  </div>
-
-  <br>
-  <br>
-      <h4 style="color: #fff; font-size: 12px;"> Preencha as informações abaixo</h4>
-      <h4 style="color: #fff; font-size: 12px; "> antes que perca a oportunidade [${toke.length}] de [1]</h4>
-  <br>
- 
-  <br>
- 
-  <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdwSdocVw4zAEvYDeaLRhBmZwGPdRdz3mU59365NeTJLF5l1A/viewform?embedded=true" width="500" height="300" frameborder="0" marginheight="0" marginwidth="0" scrolling="no" >Carregando…</iframe>
+        <br>
+        <br>
+                <a style="color: #fff; font-size: 20px;" href="${url1}">Crie o seu ticket</a>
+        
+        <br>
+        <br>
 
   `
     }
